d4bb9066dd0e5e14045a1d7a3cb8bc988d765965
Ekaterina Petukhova <katepthv12@gmail.com>
2024-04-06 19:33:59 +0200

    feat: solve getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -122,8 +122,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const promiseArr = Promise.allSettled(promises).then((responses) =>
+    responses.map((response) =>
+      response.status === 'fulfilled' ? response.value : null
+    )
+  );
+
+  return promiseArr;
 }
 
 /**
