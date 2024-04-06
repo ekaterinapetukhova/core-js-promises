052bfb2ab0ac196266745de2dc8d4f5d6ac72ba7
Ekaterina Petukhova <katepthv12@gmail.com>
2024-04-06 20:53:37 +0200

    feat: solve queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -150,8 +150,19 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  const result = [];
+  const promise = new Promise((resolve) => {
+    promises.forEach((prms, index) =>
+      prms.then((item) => {
+        result[index] = item;
+
+        if (index === promises.length - 1) resolve(result.join(''));
+      })
+    );
+  });
+
+  return promise;
 }
 
 module.exports = {
